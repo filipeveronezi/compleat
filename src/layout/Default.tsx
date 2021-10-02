f3c2559c6a2580724a2460096b830363fce07af2
Filipe Veronezi <filipeseidi@hotmail.com>
2021-10-02 09:14:36 -0300

    feat: add spacings to layout

--- src/layout/Default.tsx
+++ src/layout/Default.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 export const Layout = ({ children }: Props) => {
   return (
-    <div className="max-w-screen-xl mx-auto py-10 px-4 md:px-10">
+    <div className="max-w-screen-xl mx-auto py-10 px-8 md:px-10">
       <Header />
-      <main>{children}</main>
+      <main className="py-16 lg:py-24">{children}</main>
     </div>
   )
 }
