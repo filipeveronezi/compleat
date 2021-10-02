6b65f240249ed6f52ad78178a8c5bc5dc70bde06
Filipe Veronezi <filipeseidi@hotmail.com>
2021-10-02 12:50:00 -0300

    feat: add plans section to landing page

--- src/modules/landing/index.tsx
+++ src/modules/landing/index.tsx
@@ -1,9 +1,11 @@
 import { Hero } from './hero'
+import { Plans } from './plans'
 
 export const Landing = () => {
   return (
-    <div className="flex flex-col gap-14">
+    <div className="flex flex-col gap-32 lg:gap-48">
       <Hero />
+      <Plans />
     </div>
   )
 }
