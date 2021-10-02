a5913859e5906bb0383be6a7db6011849ca128e2
Filipe Veronezi <filipeseidi@hotmail.com>
2021-10-02 12:48:47 -0300

    refactor: width on button element

--- src/components/elements/Button/index.tsx
+++ src/components/elements/Button/index.tsx
@@ -1,10 +1,6 @@
 import { MouseEventHandler } from 'react'
 import Link from 'next/link'
 
-interface WrapperProps {
-  children: React.ReactElement
-}
-
 interface Props {
   type: 'primary' | 'secondary'
   text: string
@@ -15,7 +11,7 @@ interface Props {
 export const Button = ({ type, text, shadow, onClick }: Props) => {
   return (
     <button
-      className={`flex justify-center items-center w-32 rounded-xl h-10 px-6 py-4 font-bold cursor-pointer md:h-14 ${
+      className={`flex justify-center items-center rounded-xl w-full h-10 px-6 py-4 font-bold cursor-pointer md:h-14 ${
         type === 'primary'
           ? 'text-white bg-brand-green'
           : 'text-brand-green bg-white'
