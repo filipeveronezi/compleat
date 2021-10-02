eeab7f7c4d0ce8e2fc71e72491b384bd738285eb
Filipe Veronezi <filipeseidi@hotmail.com>
2021-10-02 11:00:29 -0300

    feat: add shadow prop to button

--- src/components/elements/Button/index.tsx
+++ src/components/elements/Button/index.tsx
@@ -8,17 +8,18 @@ interface WrapperProps {
 interface Props {
   type: 'primary' | 'secondary'
   text: string
+  shadow?: boolean
   onClick?: MouseEventHandler<HTMLButtonElement>
 }
 
-export const Button = ({ type, text, onClick }: Props) => {
+export const Button = ({ type, text, shadow, onClick }: Props) => {
   return (
     <button
       className={`flex justify-center items-center w-32 rounded-xl h-10 px-6 py-4 font-bold cursor-pointer md:h-14 ${
         type === 'primary'
           ? 'text-white bg-brand-green'
           : 'text-brand-green bg-white'
-      }`}
+      } ${shadow && 'shadow-lg'}`}
       onClick={onClick}
     >
       {text}
