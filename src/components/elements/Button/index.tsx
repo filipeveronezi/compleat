7e78ad59a0b429f96c63703431405691e56fbd0e
Filipe Veronezi <filipeseidi@hotmail.com>
2021-10-11 18:08:18 -0300

    feat: create disabled prop for Button

--- src/components/elements/Button/index.tsx
+++ src/components/elements/Button/index.tsx
@@ -6,17 +6,21 @@ interface Props {
   text: string
   shadow?: boolean
   onClick?: MouseEventHandler<HTMLButtonElement>
+  disabled?: boolean
 }
 
-export const Button = ({ type, text, shadow, onClick }: Props) => {
+export const Button = ({ type, text, shadow, onClick, disabled }: Props) => {
   return (
     <button
       className={`flex justify-center items-center rounded-xl w-full h-10 px-6 py-4 font-bold cursor-pointer md:h-14 ${
         type === 'primary'
           ? 'text-white bg-brand-green'
           : 'text-brand-green bg-white'
-      } ${shadow && 'shadow-lg'}`}
+      } ${shadow && 'shadow-lg'} ${
+        disabled && 'opacity-50 cursor-not-allowed'
+      }`}
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
