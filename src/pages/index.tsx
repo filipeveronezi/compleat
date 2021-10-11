b37f0d76329fe970a377a11e7d0c70273a6c8985
Filipe Veronezi <filipeseidi@hotmail.com>
2021-10-11 16:53:06 -0300

    feat: add title metatag to landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head'
 import { Landing } from '@/modules/Landing'
 
 const Home = () => {
-  return <Landing />
+  return (
+    <>
+      <Head>
+        <title>Compleat</title>
+      </Head>
+      <Landing />
+    </>
+  )
 }
 
 export default Home
