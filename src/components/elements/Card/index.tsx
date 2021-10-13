8406aa619f63a2eaac87f54c860897a5430d4431
Filipe Veronezi <filipeseidi@hotmail.com>
2021-10-13 20:49:29 -0300

    feat: add classname prop to card

--- src/components/elements/Card/index.tsx
+++ src/components/elements/Card/index.tsx
@@ -1,10 +1,13 @@
 interface Props {
   children: React.ReactElement
+  className?: string
 }
 
-export const Card = ({ children }: Props) => {
+export const Card = ({ children, className }: Props) => {
   return (
-    <div className="px-5 py-10 w-max bg-white shadow-card rounded-2xl">
+    <div
+      className={`${className} px-5 py-10 w-max bg-white shadow-card rounded-2xl`}
+    >
       {children}
     </div>
   )
