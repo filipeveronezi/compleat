6b4c08111082cdf53b9cf83c962bb63845311109
Filipe Veronezi <filipeseidi@hotmail.com>
2021-10-13 22:04:09 -0300

    feat: add type fields to recipe

--- src/types/Recipe.ts
+++ src/types/Recipe.ts
@@ -1,5 +1,17 @@
+interface IngredientType {
+  id: number
+  name: string
+  amount: number
+  unit: string
+  original: string
+}
+
 export interface RecipeType {
   id: number
   title: string
   image: string
+  usedIngredientCount: number
+  missedIngredientCount: number
+  usedIngredients: Array<IngredientType>
+  missedIngredients: Array<IngredientType>
 }
