041a39ee73c6cf285cb5f71d359ba32c5c6b9077
Filipe Veronezi <filipeseidi@hotmail.com>
2021-10-14 23:32:00 -0300

    feat: use overlay on _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Layout } from '@/layout/Default'
+import { useOverlay } from '@/store/useOverlay'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { overlayState, setOverlayState } = useOverlay()
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div className="w-full h-full relative">
+      {overlayState && (
+        <div className="fixed w-full h-full bg-black backdrop-filter backdrop-blur-sm bg-opacity-60 z-10" />
+      )}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   )
 }
 export default MyApp
